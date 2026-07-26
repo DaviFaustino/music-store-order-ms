60529986d635fe7f844f875a99c1ad5b7b1b74de
DaviFaustino <davifaustino.dev@gmail.com>
2026-07-25 21:50:06 -0300

    feat: add ProductProjectionService and event for product creation

---
+++ src/main/java/com/davifaustino/musicstore/order/application/projections/product/ProductCreatedEvent.java
@@ -0,0 +1,14 @@
+package com.davifaustino.musicstore.order.application.projections.product;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record ProductCreatedEvent(
+    UUID productId,
+    String sku,
+    String name,
+    String description,
+    BigDecimal price,
+    String status,
+    String type) {
+}
---
+++ src/main/java/com/davifaustino/musicstore/order/application/projections/product/ProductProjectionService.java
@@ -0,0 +1,26 @@
+package com.davifaustino.musicstore.order.application.projections.product;
+
+import org.springframework.stereotype.Service;
+
+import com.davifaustino.musicstore.order.infrastructure.outbound.persistence.projections.product.ProductProjectionRepository;
+
+@Service
+public class ProductProjectionService {
+
+    private final ProductProjectionRepository productProjectionRepository;
+
+    public ProductProjectionService(ProductProjectionRepository productProjectionRepository) {
+        this.productProjectionRepository = productProjectionRepository;
+    }
+
+    public void save(ProductCreatedEvent productCreatedEvent) {
+        productProjectionRepository.save(
+            new ProductProjectionDto(
+                productCreatedEvent.productId(),
+                productCreatedEvent.name(),
+                productCreatedEvent.price(),
+                productCreatedEvent.status()
+            )
+        );
+    }
+}
